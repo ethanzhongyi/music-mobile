bb3c43d2d807bbc8f6f84af2e5c0a73bc3431e79
zhongyi <zhongyi@giec.com>
2018-12-22 21:29:11 +0800

    初始化配置和css样式

---
+++ vue-music/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    music app
+  </div>
+</template>
+
+<script type='text/ecmascript-6'>
+
+</script>
+
+<style scopde lang='stylus' rel='stylesheet/stylus'>
+  @import '~common/stylus/variable'
+
+  #app
+    color: $color-theme
+</style>
---
+++ vue-music/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+
+//Vue.config.productionTip = false
+
+import 'common/stylus/index.styl'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
