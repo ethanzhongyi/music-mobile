41e20dfcd51a705c7e0552b76a1fa154b22dbfee
zhongyi <zhongyi@giec.com>
2018-12-23 18:11:05 +0800

    推荐页轮播图实现

--- vue-music/src/components/recommend/recommend.vue
+++ vue-music/src/components/recommend/recommend.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="recommend">
     <div class='recommend-content'>
-      <div class='slider-wrapper'>
+      <div v-if='recommends.length' class='slider-wrapper'>
+        <slider>
+          <div v-for='item in recommends'>
+          	<a :herf='item.linkUrl'>
+          	  <img :src='item.picUrl'></img>
+          	</a>
+          </div>
+        </slider>
       </div>
       <div class='recommend-list'>
         <h1 class='list-title'>热门歌单推荐</h1>
@@ -13,10 +20,16 @@
 </template>
 
 <script type='text/ecmascript-6'>
+  import Slider from 'base/slider/slider'
   import {getRecommend} from 'api/recommend'
   import {ERR_OK} from 'api/config'
 
   export default {
+    data () {
+      return {
+      	recommends : []
+      }
+    },
     created() {
       this._getRecommend()
     },
@@ -24,10 +37,13 @@
       _getRecommend() {
         getRecommend().then((res) => {
           if(res.code === ERR_OK){
-            console.log(res.data.slider)
+            this.recommends = res.data.slider
           }
         })
       }
+    },
+    components: {
+      Slider
     }
   }
 
