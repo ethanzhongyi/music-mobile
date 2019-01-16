1160a23eec6d12564d1424680b194839709413ff
zhongyi <zhongyi@giec.com>
2019-01-16 19:48:32 +0800

    歌单列表数据获取-代理host

--- vue-music/src/api/recommend.js
+++ vue-music/src/api/recommend.js
@@ -1,6 +1,8 @@
 import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
+import axios from 'axios'
 
+//通过jsonp 请求的slider轮播图的数据
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
   
@@ -11,4 +13,29 @@ export function getRecommend() {
   })
 
   return jsonp(url, data, options)
+}
+
+//通过jsonp 请求的歌单数据
+export function getDiscList() {
+  const url = '/api/getDiscList'
+
+  const data = Object.assign({}, commonParams, {
+  	platform: 'yqq.json',
+  	categoryId: 10000000,
+  	sortId: 5,
+  	picmid: 1,
+  	sin: 0,
+    ein: 29,
+    hostUin: 0,
+    needNewCode: 0,
+  	rnd: Math.random(),
+  	format: 'json'
+  })
+
+  //return jsonp(url, data, options)
+  return axios.get(url,{
+  	params: data
+  }).then((res) => {
+  	return Promise.resolve(res.data)
+  })
 }
