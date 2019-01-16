72df25b3c455fd6c2c441f7251776562f14f1ac6
zhongyi <zhongyi@giec.com>
2019-01-16 22:17:02 +0800

    scroll组件 优化

--- vue-music/src/base/scroll/scroll.vue
+++ vue-music/src/base/scroll/scroll.vue
@@ -1,11 +1,13 @@
 <template>
-  <div ref='wrapper'>
-  	<slot></slot>
+  <div ref="wrapper">
+    <slot></slot>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll'
+  const DIRECTION_H = 'horizontal'
+  const DIRECTION_V = 'vertical'
   export default {
     props: {
       probeType: {
@@ -16,9 +18,29 @@
         type: Boolean,
         default: false
       },
+      listenScroll: {
+        type: Boolean,
+        default: false
+      },
       data: {
         type: Array,
         default: null
+      },
+      pullup: {
+        type: Boolean,
+        default: false
+      },
+      beforeScroll: {
+        type: Boolean,
+        default: false
+      },
+      refreshDelay: {
+        type: Number,
+        default: 20
+      },
+      direction: {
+        type: String,
+        default: DIRECTION_V
       }
     },
     mounted() {
@@ -34,7 +56,25 @@
         this.scroll = new BScroll(this.$refs.wrapper, {
           probeType: this.probeType,
           click: this.click,
+          eventPassthrough: this.direction === DIRECTION_V ? DIRECTION_H : DIRECTION_V
         })
+        if (this.listenScroll) {
+          this.scroll.on('scroll', (pos) => {
+            this.$emit('scroll', pos)
+          })
+        }
+        if (this.pullup) {
+          this.scroll.on('scrollEnd', () => {
+            if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
+              this.$emit('scrollToEnd')
+            }
+          })
+        }
+        if (this.beforeScroll) {
+          this.scroll.on('beforeScrollStart', () => {
+            this.$emit('beforeScroll')
+          })
+        }
       },
       disable() {
         this.scroll && this.scroll.disable()
@@ -44,13 +84,19 @@
       },
       refresh() {
         this.scroll && this.scroll.refresh()
+      },
+      scrollTo() {
+        this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+      },
+      scrollToElement() {
+        this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
       }
     },
     watch: {
       data() {
         setTimeout(() => {
           this.refresh()
-        }, 20)
+        }, this.refreshDelay)
       }
     }
   }
