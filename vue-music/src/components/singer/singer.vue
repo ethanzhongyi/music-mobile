313c06beff56bbaa2ce5039a9a3d987ce5e115d0
zhongyi <zhongyi@giec.com>
2019-01-17 22:21:52 +0800

    歌手数据获取

--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -1,11 +1,86 @@
 <template>
-  <div>
+  <div class="singer">
     singer
   </div>
 </template>
 
 <script type='text/ecmascript-6'>
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+
+  const HOT_SINGER_LEN = 10
+  const HOT_NAME = '热门'
+
+  export default {
+  	data() {
+  	  return {
+  	  	singers: []
+  	  }
+  	},
+  	created() {
+  	  this._getSingerList()
+  	},
+  	methods: {
+  	  _getSingerList() {
+  	  	getSingerList().then((res) => {
+	  	  if (res.code === ERR_OK) {
+	  		this.singers = res.data.list
+	  		console.log(this._normalizeSinger(this.singers))
+	  	  }
+  	  	})
+  	  },
+  	  _normalizeSinger(list) {
+  	  	let map = {
+  	  	  hot: {
+  	  	  	title: HOT_NAME,
+  	  	  	items: []
+  	  	  }
+  	  	}
+  	  	list.forEach((item, index) => {
+  	  	  if(index < HOT_SINGER_LEN) {
+  	  	  	map.hot.items.push(new Singer({
+  	  	  	  id: item.Fsinger_mid,
+  	  	  	  name: item.Fsinger_name
+  	  	  	}))
+  	  	  }
+  	  	  //当对象的属性(key)是动态变化的就用 []
+  	  	  const key = item.Findex
+  	  	  if (!map[key]) {
+  	  	  	map[key] = {
+  	  	  	  title: key,
+  	  	  	  items: []
+  	  	  	}
+  	  	  }
+  	  	  map[key].items.push(new Singer({
+  	  	  	  id: item.Fsinger_mid,
+  	  	  	  name: item.Fsinger_name
+  	  	  	}))
+  	  	})
+  	  	// 为了得到有序列表 处理map
+  	  	let hot = []
+  	  	let ret = []
+  	  	for (let key in map) {
+  	  	  let val = map[key]
+  	  	  if(val.title.match(/[a-zA-Z]/)) {
+  	  	  	ret.push(val)
+  	  	  }else if (val.tilte === HOT_NAME) {
+  	  	  	hot.push(val)
+  	  	  }
+  	  	}
+  	  	ret.sort((a, b) => {
+  	  	  return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+  	  	})
+  	  	return hot.concat(ret)
+  	  }
+  	}
+  }
 </script>
 
 <style scopde lang='stylus' rel='stylesheet/stylus'>
+  .singer
+    positon: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
