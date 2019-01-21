781f3a5f4971857666a59dbeb9448a2959672f51
zhongyi <zhongyi@giec.com>
2019-01-21 21:22:52 +0800

    歌手详情数据处理和Song类的封装

--- vue-music/src/components/singer-detail/singer-detail.vue
+++ vue-music/src/components/singer-detail/singer-detail.vue
@@ -5,6 +5,52 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import {mapGetters} from 'vuex'
+  import {getSingerDetail} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
+
+  export default {
+  	data() {
+  	  return {
+  	  	songs: []
+  	  }
+  	},
+  	computed: {
+  	  ...mapGetters([
+  	  	'singer'
+  	  ])
+  	},
+  	created() {
+  		this._getSingerDetail()
+  	},
+  	methods: {
+  	  _getSingerDetail() {
+  	  	if(!this.singer.id) {
+  	  	  this.$router.push('/singer')
+  	  	  return
+  	  	}
+  	  getSingerDetail(this.singer.id).then((res) => {
+  	  	if (res.code === ERR_OK) {
+  	  	  this.songs = this._normalizeSongs(res.data.list)
+  	  	  console.log(this.songs)
+  	  	  }
+  	  	})
+  	  },
+  	  _normalizeSongs(list) {
+  	  	let ret = []
+  	  	list.forEach((item) => {
+  	  	  let {musicData} = item
+  	  	  if (musicData.songid && musicData.albummid) {
+  	  	    //console.log(item)
+
+  	  	  	ret.push(createSong(musicData))
+  	  	  }
+  	  	})
+  	  	return ret
+  	  }
+  	}
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
