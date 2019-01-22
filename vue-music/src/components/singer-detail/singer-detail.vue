4ad0c41fad0d98ed0f362c2c87f7a29d9aabf070
zhongyi <zhongyi@giec.com>
2019-01-22 20:46:27 +0800

    music-list组件1

--- vue-music/src/components/singer-detail/singer-detail.vue
+++ vue-music/src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :title='title' :bg-image='bgImage' :songs='songs'></music-list>
   </transition>
 </template>
 
@@ -9,6 +9,7 @@
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
 
   export default {
   	data() {
@@ -17,6 +18,12 @@
   	  }
   	},
   	computed: {
+  	  title() {
+  	  	return this.singer.name
+  	  },
+  	  bgImage() {
+  	  	return this.singer.avatar
+  	  },
   	  ...mapGetters([
   	  	'singer'
   	  ])
@@ -33,7 +40,6 @@
   	  getSingerDetail(this.singer.id).then((res) => {
   	  	if (res.code === ERR_OK) {
   	  	  this.songs = this._normalizeSongs(res.data.list)
-  	  	  console.log(this.songs)
   	  	  }
   	  	})
   	  },
@@ -42,13 +48,14 @@
   	  	list.forEach((item) => {
   	  	  let {musicData} = item
   	  	  if (musicData.songid && musicData.albummid) {
-  	  	    //console.log(item)
-
   	  	  	ret.push(createSong(musicData))
   	  	  }
   	  	})
   	  	return ret
   	  }
+  	},
+  	components: {
+  	  MusicList
   	}
   }
 </script>
