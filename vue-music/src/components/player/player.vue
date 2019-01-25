21f32ff9ac2e1adcf40e1f238098e51a9deaab74
zhongyi <zhongyi@giec.com>
2019-01-25 21:19:54 +0800

    播放器html搭建

--- vue-music/src/components/player/player.vue
+++ vue-music/src/components/player/player.vue
@@ -1,9 +1,59 @@
 <template>
   <div class="player" v-show='playlist.length'>
     <div class="normal-player" v-show='fullScreen'>
-      bo fang qi
+      <div class="background">
+          <img width="100%" height="100%">
+      </div>
+      <div class="top">
+        <div class="back">
+          <i class="icon-back"></i>
+        </div>
+        <h1 class="title"></h1>
+        <h2 class="subtitle"></h2>
+      </div>
+      <div class="middle">
+        <div class="middle-l">
+          <div class="cd-wrapper">
+            <div class="cd">
+              <img class="image">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="operators">
+          <div class="icon i-left">
+            <i class="icon-sequence"></i>
+          </div>
+          <div class="icon i-left">
+            <i class="icon-prev"></i>
+          </div>
+          <div class="icon i-center">
+            <i class="icon-play"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-next"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-not-favorite"></i>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="mini-player" v-show='!fullScreen'></div>
+    <div class="mini-player" v-show='!fullScreen'>
+      <div class="icon">
+        <img width="40" height="40">
+      </div>
+      <div class="text">
+        <h2 class="name"></h2>
+        <p class='desc'></p>
+      </div>
+      <div class="control">
+      </div>
+      <div class="control">
+        <i class="icon-playlist"></i>
+      </div>
+    </div>
   </div>
 </template>
 
