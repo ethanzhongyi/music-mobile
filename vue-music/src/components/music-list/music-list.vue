adb14ef3dc58fff26b96abf4c7d32af12bff9e2a
zhongyi <zhongyi@giec.com>
2019-02-14 20:35:15 +0800

    使用mixin去解决各页面底部播放器的高度自适应

--- vue-music/src/components/music-list/music-list.vue
+++ vue-music/src/components/music-list/music-list.vue
@@ -31,12 +31,14 @@
   import {prefixStyle} from 'common/js/dom'
   import Loading from 'base/loading/loading'
   import {mapActions} from 'vuex'
+  import {playlistMixin} from 'common/js/mixin'
 
   const RESERVED_HEIGHT = 40
   const transform = prefixStyle('transform')
   const backdrop = prefixStyle('backdrop-filter')
 
   export default {
+    mixins: [playlistMixin],
     props: {
       bgImage: {
         type: String,
@@ -72,6 +74,11 @@
       this.$refs.list.$el.style.top = `${this.imageHeight}px`
     },
     methods: {
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px' : '0'
+        this.$refs.list.$el.style.bottom = bottom //如果是vue 组件 的话就需要加$el
+        this.$refs.list.refresh()
+      },
       scroll(pos) {
         this.scrollY  = pos.y
       },
