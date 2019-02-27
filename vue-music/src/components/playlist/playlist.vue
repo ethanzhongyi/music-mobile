4c051cf6ca97fc5afef081a8cb9eaa8c1f5ad889
zhongyi <zhongyi@giec.com>
2019-02-27 16:08:28 +0800

    add-song 添加歌曲到列表组件

--- vue-music/src/components/playlist/playlist.vue
+++ vue-music/src/components/playlist/playlist.vue
@@ -24,7 +24,7 @@
           </transition-group>
         </scroll>
         <div class="list-operate">
-          <div class="add">
+          <div class="add" @click='showAddSong'>
             <i class="icon-add"></i>
             <span class="text">添加歌曲到队列</span>
           </div>
@@ -34,6 +34,7 @@
         </div>
       </div>
       <confirm ref='confirm' @confirm='confirmClear' text='是否清空播放列表' confirmBtnText='清空'></confirm>
+      <add-song ref='addsong'></add-song>
     </div>
   </transition>
 </template>
@@ -44,6 +45,7 @@
   import {playMode} from 'common/js/config'
   import Confirm from 'base/confirm/confirm'
   import {playerMixin} from 'common/js/mixin'
+  import AddSong from 'components/add-song/add-song'
 
   export default {
     mixins: [playerMixin],
@@ -105,6 +107,9 @@
         this.deleteSongList()
         this.hide()
       },
+      showAddSong() {
+        this.$refs.addsong.show()
+      },
       ...mapMutations({
         setCurrentIndex:'SET_CURRENT_INDEX',
         setPlayingState:'SET_PLAYING_STATE'
@@ -124,7 +129,8 @@
     },
     components: {
       Scroll,
-      Confirm
+      Confirm,
+      AddSong
     }
   }
 </script>
